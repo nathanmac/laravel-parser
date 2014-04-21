824fd007112828130a605b7ed15dc93239691ced
Nathan Macnamara <nathan.macnamara@arrowecs.co.uk>
2014-04-21 20:58:09 +0100

    Add additional variations of Content-Type values, plus add support for yaml.

--- src/config/config.php
+++ src/config/config.php
@@ -5,9 +5,22 @@ return array(
 	'default' => 'json',
 
 	'supported_formats' => array(
+// XML
 	    'application/xml' => 'xml',
+	    'text/xml' => 'xml',
+// JSON
 	    'application/json' => 'json',
-	    'application/vnd.php.serialized' => 'serialize',
+		'application/x-javascript' => 'json',
+		'text/javascript' => 'json',
+		'text/x-javascript' => 'json',
+		'text/x-json' => 'json',
+// YAML
+	    'text/yaml' => 'yaml',
+		'text/x-yaml' => 'yaml',
+		'application/yaml' => 'yaml',
+		'application/x-yaml' => 'yaml',
+// MISC
+		'application/vnd.php.serialized' => 'serialize',
 	    'application/x-www-form-urlencoded' => 'querystr'
 	)
 );
